449d3d1986d0eb9ff9c9cde8fb1e3f12baba5278
unknown <chamimeriem2302@gmail.com>
2024-02-23 20:00:14 +0100

    add-combined-func-taln-file

--- TP TALN.py
+++ TP TALN.py
@@ -1,21 +1,39 @@
 from arabic_reshaper import reshape
 from bidi.algorithm import get_display
+import os
+import random
 
 #first inqtall pip install arabic-reshaper ,pip install python-bidi
 
 class ArabicTextProcessor:
-    def __init__(self, text_path, SW_Path):
-        self.text_path = text_path
+    def __init__(self, folder_path, SW_Path,number_of_files):
+        self.folder_path = folder_path
         self.SW_Path = SW_Path
-        self.text_content = ""
+        self.number_of_files = number_of_files
+        self.combined_content = ""
         self.sw_content = ""
         self.distinct_words = []
         self.stop_words = []
 
-    def read_files(self):
+
+    def combine_all_text_files(self):
+    # Get all file names in the folder
+
+        all_files = [f for f in os.listdir(self.folder_path) if f.endswith('.txt')]
+        # Randomly select the specified number of text files
+        selected_files = random.sample(all_files, min(len(all_files), self.number_of_files))
+        print(len(selected_files))
+        # Loop through each selected file, read its content, and append to the combined_content variable
+        for file_name in selected_files:
+            file_path = os.path.join(self.folder_path, file_name)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+                # Append the file content to the combined_content variable, separated by a space
+                self.combined_content += content + " "
+
+
+    def read_sw_file(self):
         try:
-            with open(self.text_path, 'r', encoding='utf-8') as file:
-                self.text_content = file.read()
             with open(self.SW_Path, 'r', encoding='utf-8') as file:
                 self.sw_content = file.read()
         except FileNotFoundError:
@@ -26,7 +44,7 @@ class ArabicTextProcessor:
     
     def get_words(self,content,separation = " "):
         '''a function for splitting files to list'''
-        return self.text_content.split(separation)
+        return self.combined_content.split(separation)
 
     def get_rid_sw(self):
         '''a function delete stop words'''
@@ -43,17 +61,18 @@ class ArabicTextProcessor:
         pass
 
     def processing(self):
-        self.read_files()
-        self.distinct_words = self.get_words(self.text_content)
+        self.combine_all_text_files()
+        self.read_sw_file()
+        self.distinct_words = self.get_words(self.combined_content)
         self.stop_words = self.get_words(self.sw_content,"\n")
         self.get_remove_number()
         self.get_rid_sw()
         self.distinct_words = [get_display(reshape(word)) for word in self.distinct_words]
 
-
-text_path = "C:/Users/meriem/Documents/vs/tp_m1/nlp/Sports/0000.txt"
 SW_Path = "C:/Users/meriem/Documents/vs/tp_m1/nlp/arabic_text_analyser_nlp/list.txt"
-test = ArabicTextProcessor(text_path, SW_Path)
-test.read_files()
+folder_path = os.path.join(os.path.dirname(__file__), 'Sports')
+number_of_files = 2
+test = ArabicTextProcessor(folder_path, SW_Path,number_of_files)
 test.processing()
 print(test.distinct_words)
+print(len(test.distinct_words))
