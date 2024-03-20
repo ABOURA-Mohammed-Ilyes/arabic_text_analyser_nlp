c87817ff4a64748ca72741405505ca90403bc747
aboura mohammed ilyes <abouramohammedilyes@gmail.com>
2024-03-20 17:30:31 +0100

    some minor changes

--- new_function.py
+++ new_function.py
@@ -1,11 +1,9 @@
 from arramooz.arabicdictionary import ArabicDictionary
 from farasa.stemmer import FarasaStemmer
-from farasa.ner import FarasaNamedEntityRecognizer
 import os
 import json
 import qalsadi.lemmatizer 
 import re
-import ner_module
 
 # pip install qalsadi pip  install farasapy
 
@@ -37,16 +35,6 @@ def dict_test(content):
       if len(verbs_list)==0 and len(nouns_list)==0:
           unknown_words.append(word)
 
-      # else:
-      #   if verbs_list:
-      #       for word_tuple in verbs_list:
-      #           word_info = dict(word_tuple)
-      #           print(f"Root (verb): {word_info['root']}")
-      #   if nouns_list:
-      #       for word_tuple in nouns_list:
-      #           word_info = dict(word_tuple)
-      #           print(f"Root (noun): {word_info['root']}")    
-
   return unknown_words       
 
 def stem_word(content):
@@ -71,43 +59,23 @@ def type_word(content):
 
     return words_with_all        
 
-
-
-def ner_word(content):
-    named_entity_recognizer_interactive = FarasaNamedEntityRecognizer()
-    ner_words_unknown = []
-
-    for word in content:
-        print(word)
-        ner_word = named_entity_recognizer_interactive.recognize(word)
-        print(ner_word)
-        if "O" in ner_word:
-            ner_words_unknown.append(word)
-    named_entity_recognizer_interactive.terminate()    
-    return ner_words_unknown
-
-
-
            
 
 # Supprimer les mots qui ne sont pas en arabe
-# data = filter_no_arabic(data) 
-# stemmed_words_unknown = stem_word(data)
-# unknown_words = dict_test(stemmed_words_unknown)
-# words_with_all = type_word(unknown_words)
+data = filter_no_arabic(data) 
+stemmed_words_unknown = stem_word(data)
+unknown_words = dict_test(stemmed_words_unknown)
+words_with_all = type_word(unknown_words)
 
-# with open("words_with_all.json","w",encoding='utf-8') as j:
-#     json.dump(words_with_all,j,ensure_ascii=False)
+with open("unknown_words.json","w",encoding='utf-8') as j:
+    json.dump(words_with_all,j,ensure_ascii=False)
 
    
-# ner_words_unknown = ner_word(unknown_words)
-# print(ner_words_unknown)
 # print(words_with_all)
-# print(len(stemmed_words_unknown))
-# print(len(unknown_words))
-# print(len(words_with_all))
+print(len(stemmed_words_unknown))
+print(len(unknown_words))
+print(len(words_with_all))
 
-# print(len(ner_words_unknown))
     
 
 
